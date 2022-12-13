f272377c2659ada893b026fe774971293a697c02
吴习章 <wuxz14@vanke.com>
2022-12-13 19:43:50 +0800

    feat: change

---
+++ src/AppDemo.vue
@@ -0,0 +1,32 @@
+<script setup>
+  // import { VButton, VButton01 } from './components';
+</script>
+
+<template>
+  <VButton
+    class="
+      py-2
+      px-4
+      font-semibold
+      rounded-lg
+      shadow-md
+      text-white
+      bg-green-500
+      hover:bg-green-700
+    "
+    >My Button</VButton
+  >
+    <VButton01
+    class="
+      py-2
+      px-4
+      font-semibold
+      rounded-lg
+      shadow-md
+      text-white
+      bg-green-500
+      hover:bg-green-700
+    "
+    >My Button01</VButton01
+  >
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './assets/index.css';
+// import { createApp } from 'vue';
+// import App from './App.vue';
+// import './assets/index.css';
+// createApp(App).mount('#app');
 
-createApp(App).mount('#app');
+
+import { VButton, VButton01 } from 'vite-library-suimu';
+import { createApp } from 'vue';
+import AppDemo from './AppDemo.vue';
+// import './assets/index.css';
+const app = createApp(AppDemo);
+app.use(VButton);
+app.use(VButton01);
+app.mount('#app')
+
+
+

0e90bc73a3848fdf229ec88b1e10aca96873cfc5
吴习章 <wuxz14@vanke.com>
2022-12-14 12:01:20 +0800

    feat: change

--- src/components/index.js
+++ src/components/index.js
@@ -3,22 +3,13 @@
 
 import VButton from './VButton.vue'
 import VButton01 from './VButton01.vue'
-import _Vue from 'vue'
 
 VButton.install = (Vue) => {
-  if (!Vue) {
-    window.Vue = Vue = _Vue
-  } else {
     Vue.component(VButton.name, VButton)
-  }
 }
 
 VButton01.install = (Vue) => {
-  if (!Vue) {
-    window.Vue = Vue = _Vue
-  } else {
     Vue.component(VButton01.name, VButton01)
-  }
 }
 
 export { VButton, VButton01 }

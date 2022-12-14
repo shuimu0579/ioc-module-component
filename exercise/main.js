4cd84eed4a021e1ef8b50d96dd4c67e901d62f84
吴习章 <wuxz14@vanke.com>
2022-12-14 20:48:27 +0800

    feat: change

--- exercise/main.js
+++ exercise/main.js
@@ -2,12 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 // 打包好的组件--本地测试
-import IOCModuleComponent from '../dist/my-lib.es.js';
-import '../dist/style.css';
+// import IOCModuleComponent from '../dist/my-lib.es.js';
+// import '../dist/style.css';
 
 // 打包好的组件--npm包引入测试
-// import IOCModuleComponent from 'ioc-module-component';
-// import '../node_modules/ioc-module-component/dist/style.css';
+import IOCModuleComponent from 'ioc-module-component';
+import '../node_modules/ioc-module-component/dist/style.css';
 
 const app = createApp(App);
 app.use(IOCModuleComponent);

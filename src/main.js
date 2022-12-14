9f14a39c978cf398ceffb3a6150c6929e7431552
吴习章 <wuxz14@vanke.com>
2022-12-14 14:53:40 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-createApp(App).mount('#app');
-
-
-// import { VButton, VButton01 } from 'vite-library-suimu';
-// import 'vite-library-suimu/dist/style.css';
 // import { createApp } from 'vue';
-// import AppDemo from './AppDemo.vue';
-// const app = createApp(AppDemo);
-// app.use(VButton);
-// app.use(VButton01);
-// app.mount('#app')
+// import App from './App.vue';
+// createApp(App).mount('#app');
+
+
+import TUI from 'vite-library-suimu';
+import '../node_modules/vite-library-suimu/dist/style.css';
+import { createApp } from 'vue';
+import AppDemo from './AppDemo.vue';
+const app = createApp(AppDemo);
+app.use(TUI);
+app.mount('#app')
 
 
